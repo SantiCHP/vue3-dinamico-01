d7c04c6a7feeaf978d5bf25aba5f507492beafc9
SantiCHP <santichadmin@gmail.com>
2022-08-29 00:52:46 +0100

    componentes y props

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,35 @@
 <script setup>
-import ButtonCounter from "./components/ButtonCounter.vue";
+import { ref } from "vue"
+import BlogPost from "./components/BlogPost.vue";
+
+const posts = ref([
+  { title: 'Post 1', id: 1, body: 'Descripción 1' },
+  { title: 'Post 2', id: 2, body: 'Descripción 2' },
+  { title: 'Post 3', id: 3, body: 'Descripción 3' },
+  { title: 'Post 4', id: 4 },
+]);
+
+const favorito = ref('');
+
+const cambiarFavorito = (title) => {
+  favorito.value = title;
+};
 
 </script>
 
 <template>
   <div class="container mt-2">
     <h1>APP</h1>
-    <ButtonCounter />
-    <ButtonCounter />
-    <ButtonCounter />
-    <ButtonCounter />
-    <ButtonCounter />
-    <ButtonCounter />
-    <ButtonCounter />
+    <h2>Mi post favorito: {{ favorito }}</h2>
+
+    <BlogPost
+      v-for="post in posts"
+      :key="post.id"
+      :title = "post.title"
+      :id = "post.id"
+      :body = "post.body"
+      @cambiarFavorito="cambiarFavorito"
+    ></BlogPost>
   </div>
-
 </template>
-
 <style></style>
---
+++ src/components/BlogPost.vue
@@ -0,0 +1,35 @@
+<script setup>
+
+// defineProps(['title', 'id', 'body']);
+
+// String, Number, Boolean, Array, Object, Date, Function, Symbol
+defineProps({
+    title: String,
+    id: Number,
+    body: {
+        type: String,
+        default: 'Sin descripción'
+    }
+})
+
+</script>
+
+<template>
+    <div class="card mt-2">
+        <div class="card-body">
+            <h5 class="card-title">{{ id }} - {{ title }}</h5>
+            <p>{{body}}</p>
+            <button 
+                class="btn btn-outline-primary" 
+                @click="$emit('cambiarFavorito', title)" 
+                data-toggle="tooltip"
+                data-placement="top" 
+                data-animation="fade"
+                data-animation-delay="0.5s"
+                title="Favorito"
+            >
+                ❤️
+            </button>
+        </div>
+    </div>
+</template>
