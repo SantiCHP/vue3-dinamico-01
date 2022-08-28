54c702adbc230db7b8e9e1eee511718d7ee446f6
SantiCHP <santichadmin@gmail.com>
2022-08-28 01:20:19 +0100

    primer commit en esta rama

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,20 @@
 <script setup>
-import { ref, computed } from "vue";
-const name = "Vue 3 dinámico";
-
-const counter = ref(0);
-const arrayCounter = ref([]);
-
-const increment = () => counter.value++;
-const decrement = () => counter.value--;
-const reset = () => (counter.value = 0);
-const add = () => arrayCounter.value.push(counter.value);
-
-const blockNumber = computed(() => {
-  const number = arrayCounter.value.find((num) => num === counter.value);
-  return number || number === 0;
-})
-
-const classCounter = computed(() => {
-  if (counter.value === 0) {
-    return "text-dark";
-  }
-
-  return counter.value > 0 ? "text-success" : "text-danger";
-});
+import ButtonCounter from "./components/ButtonCounter.vue";
 
 </script>
 
-<template class="container">
-  <div class="d-flex flex-column align-items-center justify-content-center mt-3">
-    <h1 class="text-danger">Hey! {{ name.toUpperCase() }}</h1><br>
-    <h2 :class="classCounter">{{ counter }}</h2>
-    <div class="btn-group mt-4">
-      <button class="btn btn-primary mx-2" @click="increment">Aumentar</button>
-      <button class="btn btn-warning mx-2" @click="decrement">Disminuir</button>
-      <button class="btn btn-danger mx-2" @click="reset">Resetear</button>
-      <button class="btn btn-info mx-2" @click="add" :disabled="blockNumber">Añadir</button>
-    </div>
-    <div class="d-flex justify-content-center">
-      <ul class="list-group fs-2 mt-4 text-center">
-        <li class="list-group-item" v-for="(number, index) in arrayCounter" :key="index">
-          {{ number }}
-        </li>
-      </ul>
-    </div>
-
+<template>
+  <div class="container mt-2">
+    <h1>APP</h1>
+    <ButtonCounter />
+    <ButtonCounter />
+    <ButtonCounter />
+    <ButtonCounter />
+    <ButtonCounter />
+    <ButtonCounter />
+    <ButtonCounter />
   </div>
+
 </template>
 
 <style></style>
---
+++ src/components/ButtonCounter.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { ref } from "vue";
+
+const counter = ref(0);
+const increment = () => counter.value++;
+</script>
+
+<template>
+    <button @click="increment" class="btn btn-secondary mx-2">Counter {{ counter }}</button>
+</template>
+
